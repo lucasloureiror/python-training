2a3168779d4047e0d510bcd849db08df8b08dd17
Lucas <prolucasloureiro@gmail.com>
2022-07-27 17:02:02 -0300

    Terminei a parte de saida

---
+++ Básico/saida.py
@@ -0,0 +1,32 @@
+print('Linha 1')  #É assim que eu imprimo alguma coisa em Python.
+
+print('Linha 2 em nova linha')  #Cada print vai para outra linha
+
+print(123456)  #Aqui eu consigo imprimir diretamente um número
+
+print ('Luiz', 'Otávio', 'Outra coisa')  #Separando por espaços
+
+
+#Escolhendo a separação entre os argumentos da função print
+
+print ('Luiz', 'Otávio', 'Outra coisa', sep ='-')
+
+#O separador se torna o traço
+
+#Acabando com a quebra de linha do print
+
+print('Mostrando a quebra de linha indo embora', end='')
+print('Teste na mesma linha')
+
+#O segundo print vai ficar na mesma linha do primeiro e posso escolher o que vai substituir a quebra de linha
+
+#Brincando com sep e end
+
+print('824', '176', '070', sep='.', end='-')
+print('18')
+
+#Vai sair tudo na mesma linha com pontos e traços no lugar da quebra de linha
+
+
+
+
