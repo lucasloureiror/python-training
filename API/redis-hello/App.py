32df973d1f618c58f8c16a19e7ed0033c845e1a7
Lucas <prolucasloureiro@gmail.com>
2023-01-26 13:54:01 -0300

    feat: dockerfile for API with redis

---
+++ API/redis-hello/App.py
@@ -0,0 +1,27 @@
+import redis
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+host = redis.Redis(host="localhost", port=6379)
+
+if host.exists("counter") == False:
+    host.set("counter", 0)
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    contador = host.get("counter")
+    host.incr("counter")
+    return """
+       <html>
+        <head>
+            <title>Contador do Lucas</title>
+        </head>
+        <body>
+            <h2>Você já entrou nesse site {} vezes.</h2>
+        </body>
+    </html>
+    """.format(contador)
+    
+     
+
